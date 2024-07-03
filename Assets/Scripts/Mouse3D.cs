e291457f32ea255db0e9e0d4e0eae6727e5b9348
Trinh Gia Khang <Khangtg@ebizworld.com.vn>
2024-07-03 17:26:11 +0700

    Mouse Point 3D

---
+++ Assets/Scripts/Mouse3D.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Mouse3D : MonoBehaviour
+{
+    [SerializeField] Camera _camera;
+    [SerializeField] LayerMask _layerMask;
+    [SerializeField] bool isSpecificLayerMask;
+
+
+    private void Update()
+    {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        bool hit;
+
+        if (isSpecificLayerMask)
+        {
+            hit = Physics.Raycast(ray, out hitInfo, float.MaxValue, _layerMask);
+        }
+        else
+        {
+            hit = Physics.Raycast(ray, out hitInfo);
+        }
+
+        if (hit)
+        {
+            transform.position = hitInfo.point;
+        }
+    }
+
+}
